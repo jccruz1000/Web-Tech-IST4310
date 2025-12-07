2570a48b96ba1f8ae9ca6ab953159f873c7d3fcc
jccruz1000 <007044304@coyote.csusb.edu>
2025-12-06 16:02:59 -0800

    Delete safebyte/pages/marketing.php

--- safebyte/pages/marketing.php
+++
@@ -1,9 +0,0 @@
-<section class="hero-section">
-    <h1>WELCOME TO SAFEBYTE</h1>
-    <h2>THE ULTIMATE DEFENSE AGAINST A SURVEILLED WORLD.</h2>
-    <p>SAFEBYTE provides innovative, user-friendly solutions to secure VPN gateways—empowering you to safeguard personal data and physical spaces.</p>
-    <a href="index.php?page=pricing" class="btn-primary">EXPLORE SOLUTIONS</a>
-    
-
-
-</section>
